9721ddca88e6ae79cd7896f7039e61a5efc0e83b
Michael <settees_folks2k@icloud.com>
2025-03-26 01:57:56 +0100

    Create indecisive.js

---
+++ indecisive.js
@@ -0,0 +1,3 @@
+const answers = ["🤔 Maybe?", "🙃 I don't know.", "🤷‍♂️ Ask again later.", "🤨 That's a tough one."];
+
+console.log("🔮 Answer: " + answers[Math.floor(Math.random() * answers.length)]);
